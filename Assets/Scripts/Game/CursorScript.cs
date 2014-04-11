e4e9a401a39eea62b528efb5ed6c5de79e571140
Josh Collins <jcoll007@ucr.edu>
2014-04-10 19:36:15 -0700

    Added controls by xbox controller, as well as added ability cooldowns

---
+++ Assets/Scripts/Game/CursorScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorScript : MonoBehaviour {
+
+	private Vector3 previousMousePosition;
+
+	// Use this for initialization
+	void Start () {
+		previousMousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Input.mousePosition
+	}
+}
