ebe42332ef82e1fae0ca8249d63e1d1767074b81
Victor Barrera <vbarr004@ucr.edu>
2014-06-10 15:05:23 -0700

    Game Over Screen

--- Assets/Scripts/GameOverGUI.cs
+++ Assets/Scripts/GameOverGUI.cs
@@ -11,13 +11,17 @@ public class GameOverGUI : MonoBehaviour {
 	private static string HighScoreString;
 	private static int StringWidth = 200;
 	private static int StringHeight = 50;
-
+	public Texture black;
+	public Texture back;
 	private bool newHighScore = false;
 
 	//Style
 	private Font font;
+	private Font fontUnifrak;
 	private string fontPath = "Fonts/FreePixel";
+	private string fontPathUnifrak = "Fonts/UnifrakturCook";
 	private GUIStyle style;
+	private GUIStyle styleUnifrak;
 
 	// Use this for initialization
 	void Start ()
@@ -26,12 +30,19 @@ public class GameOverGUI : MonoBehaviour {
 
 		//load our font
 		font = Resources.Load<Font>(fontPath);
+		fontUnifrak = Resources.Load<Font> (fontPathUnifrak);
 
 		//set up our normal style
 		style = new GUIStyle();
 		style.fontSize = 60;
 		style.font = font;
-		style.normal.textColor = Color.black;
+		style.normal.textColor = Color.white;
+
+		//set up our unifrak style
+		styleUnifrak = new GUIStyle();
+		styleUnifrak.fontSize = 80;
+		styleUnifrak.font = fontUnifrak;
+		styleUnifrak.normal.textColor = Color.white;
 
 		//figure out our score and high score
 		if (PlayerPrefs.GetFloat("Score") > PlayerPrefs.GetFloat("HighScore"))
@@ -62,11 +73,27 @@ public class GameOverGUI : MonoBehaviour {
 
 	void OnGUI()
 	{
+		// Background
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), black);
+		float locx = 0.0f;
+		float locy = 0.0f;
+		if(Screen.width > 1271){
+			float spacex = (Screen.width - 1271)/2.0f;
+			locx = spacex;
+			float spacey = (Screen.height-725)/2.0f;
+			locy = spacey;
+			GUI.DrawTexture (new Rect (locx, locy, 1271, 725), back, ScaleMode.ScaleAndCrop);
+		}
+		else{
+			GUI.DrawTexture (new Rect (0, 0, 1271, 725), back, ScaleMode.ScaleAndCrop);
+		} 
+
+		// Score
 		int x = (Screen.width / 2) - (StringWidth / 2);
 		int y = 50;
-		GUI.Label(new Rect(x, y, StringWidth, StringHeight), GameOverString, style);
-		GUI.Label(new Rect(x, y + 200, StringWidth, StringHeight), HighScoreString, style);
-		GUI.Label(new Rect(x, y + 300, StringWidth, StringHeight), ScoreString, style);
-		GUI.Label(new Rect(x, y + 400, StringWidth, StringHeight), ContinueString, style);
+		GUI.Label(new Rect(locx+(Screen.width/2.0f)-175, y, StringWidth, StringHeight), GameOverString, styleUnifrak);
+		GUI.Label(new Rect(locx+(Screen.width/2.0f)-235, y + 200, StringWidth, StringHeight), HighScoreString, style);
+		GUI.Label(new Rect(locx+(Screen.width/2.0f)-155, y + 300, StringWidth, StringHeight), ScoreString, style);
+		GUI.Label(new Rect(locx+(Screen.width/2.0f)-275, y + 500, StringWidth, StringHeight), ContinueString, style);
 	}
 }
